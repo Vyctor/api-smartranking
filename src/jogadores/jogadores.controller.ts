d4b38c3b18926a482f345f6559effcca71430769
Vyctor <dev.vyctor@gmail.com>
2021-05-12 12:24:39 -0300

    feat: finalizando service de jogadores

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,15 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { JogadoresService } from './jogadores.service';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
+  constructor(private readonly jogadoresService: JogadoresService) {}
+
   // TODO: verificar se vai ficar assim, caso sim, refatorar para criar e atualizar separadamente
   @Post()
   async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
-    const { nome, email } = criarJogadorDto;
-    return JSON.stringify({
-      nome,
-      email,
-    });
+    await this.jogadoresService.criarAtualizarJogador(criarJogadorDto);
   }
 }
--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { Jogador } from './interfaces/jogador.interface';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
-export class JogadoresService {}
+export class JogadoresService {
+  private jogadores: Jogador[] = [];
+  private readonly logger = new Logger(JogadoresService.name);
+
+  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
+    await this.criar(criarJogadorDto);
+  }
+
+  private async criar(criarJogadorDto: CriarJogadorDto): Promise<void> {
+    const { nome, email, telefoneCelular } = criarJogadorDto;
+
+    const jogador: Jogador = {
+      _id: uuid(),
+      nome,
+      telefoneCelular,
+      email,
+      ranking: 'A',
+      posicaoRanking: 1,
+      urlFoto: 'https://google.com',
+    };
+    this.logger.log(`criaJogadorDto: ${JSON.stringify(jogador)}`);
+    this.jogadores.push(jogador);
+  }
+}
