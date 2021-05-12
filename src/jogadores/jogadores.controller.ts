3f511a066b66527ba43ef158705b03ce6fb11bb4
Vyctor <dev.vyctor@gmail.com>
2021-05-12 11:41:19 -0300

    feat: criando controller de jogadores - parte 1

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('api/v1/jogadores')
-export class JogadoresController {}
+export class JogadoresController {
+  // TODO: verificar se vai ficar assim, caso sim, refatorar para create and update separadamente
+  @Post()
+  async criarAtualizarJogador() {
+    return JSON.stringify({
+      nome: 'Vyctor',
+    });
+  }
+}
