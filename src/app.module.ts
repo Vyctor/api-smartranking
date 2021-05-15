e94a23d5cd3d5ff99b7771943296f43def9ce842
Vyctor <dev.vyctor@gmail.com>
2021-05-15 13:00:36 -0300

    feat: importado Mongoose com configuracoes do banco no app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.kbn5i.mongodb.net/smartranking?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+      },
+    ),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
