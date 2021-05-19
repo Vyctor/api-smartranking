30e3b5aed60b5d86da56c40410501e7465dfd2df
Vyctor <dev.vyctor@gmail.com>
2021-05-18 21:50:58 -0300

    feat: entidade categorias - parte 1

--- src/categorias/categorias.module.ts
+++ src/categorias/categorias.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CategoriasController } from './categorias.controller';
 import { CategoriasService } from './categorias.service';
+import { JogadoresModule } from '../jogadores/jogadores.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriaSchema } from './interfaces/categoria.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Categoria', schema: CategoriaSchema }]),
+    JogadoresModule,
+  ],
   controllers: [CategoriasController],
-  providers: [CategoriasService]
+  providers: [CategoriasService],
 })
 export class CategoriasModule {}
