737b4324b5632060405a661e01b37b8f7f1582fd
Vyctor <dev.vyctor@gmail.com>
2021-05-25 13:23:00 -0300

    feat: adicionando moment timezone e tratamento para recuperar datas no timezone America/Sao_Paulo

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { format } from 'date-fns-tz';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  Date.prototype.toJSON = function (): any {
+    return format(this, 'YYYY-MM-DD HH:mm:ss.SSS', {
+      timeZone: 'America/Sao_Paulo',
+    });
+  };
   await app.listen(8080);
 }
 bootstrap();
