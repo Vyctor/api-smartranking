4aabc0246d9d0ece9bb97706965fe7e4885d3540
Vyctor <dev.vyctor@gmail.com>
2021-11-21 21:26:38 -0300

    movendo link de acesso ao banco de dados para variavel de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { CategoriasModule } from './categorias/categorias.module';
@@ -6,15 +7,15 @@ import { DesafiosModule } from './desafios/desafios.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:admin@cluster0.kbn5i.mongodb.net/smartranking?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify: true,
-      },
-    ),
+    ConfigModule.forRoot({
+      envFilePath: '.production.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+    }),
     JogadoresModule,
     CategoriasModule,
     DesafiosModule,
